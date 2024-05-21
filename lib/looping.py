c9cda98aace8d0eb5a8e98a092b362acdcc64e23
WendyKariuki <phaminuswendy@gmail.com>
2024-05-21 18:41:44 +0300

    Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")   
     pass
+    
 
 def square_integers(int_list):
-    # code goes here!
+    return [i ** 2 for i in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    def fizzbuzz():
+        for num in range(1, 101):
+            if num % 3 == 0 and num % 5 == 0:
+                print("FizzBuzz")
+            elif num % 3 == 0:
+                print("Fizz")
+            elif num % 5 == 0:
+                print("Buzz")
+            else:
+                print(num)
+    fizzbuzz()
     pass
